bfaf0705567d0cf979febefc9295caf151f5eb3a
Jonathan Dunne <dunnej1@student.unimelb.edu.au>
2018-10-07 01:17:50 +1000

    updated app.js

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
-const express = require("express");
-const app = express();
-
-const db = require("./models/db");
+const database = require("./models/db");
+var db = mongoose.connection;
 const routes = require("./routes/routes");
 const PORT = process.env.PORT || 3000;
 
-// var session = require("express-session");
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
+});
+
+//use sessions for tracking logins
+app.use(session({
+    secret: 'work hard',
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongooseConnection: db
+    })
+}));
 
 
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
 
@@ -17,6 +38,21 @@ app.use("/css", express.static(__dirname + '/css'));
 app.use("/img", express.static(__dirname + '/img'));
 app.use("/js", express.static(__dirname + '/js'));
 app.use("/", routes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('File Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.send(err.message);
+});
+
 
 app.listen(PORT, () => {
     console.log(`Express listening on port ${PORT}`);
