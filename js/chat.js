b3be42f24697a231ab707a5c1f53516860e44b4f
Maxwell <maxscaddie@gmail.com>
2018-10-07 14:14:52 +1100

    Final chat text

--- js/chat.js
+++ js/chat.js
@@ -1,5 +1,7 @@
-var chat_messages = ["Hey Max","I'm doing great, how are you?", "I love my job, I love my kids" +
-" sometimes working Google makes it hard to balance both.", "BMW"];
+var chat_messages = ["Hey Dan","I'm great", "I love my job, Computing and marketing are crucial parts of my " +
+"job. I personally wasn't super interested in computing and I had to work hard to learn to love it" ,
+    "My favourite part of my job is that I can work really hard at work and then go home and focus on my family",
+    "Just enjoy everything","Your welcome, reach out to me anytime! :)"];
 var counter = 0;
 
 //Adds listener to the chat box
@@ -15,12 +17,12 @@ document.getElementById("chat_box").addEventListener("keyup", function(event) {
 
 function add_to_chat() {
     //Get the chat line and add the text in the chat box
-    document.getElementById("chat").innerHTML += "<b>"+document.getElementById("chat_box").value + "</b><br>";
+    document.getElementById("chat").innerHTML += document.getElementById("chat_box").value + "<br>";
     //Clear the chat box
     document.getElementById("chat_box").value = "";
 
     //Time before random response
-    var timer = (chat_messages[counter].length+2)*100;
+    var timer = (Math.min(chat_messages[counter].length+2,40)*70);
 
     //Make the other user "respond"
     setTimeout(random_reponse,timer);
@@ -30,5 +32,5 @@ function add_to_chat() {
 };
 
 function random_reponse(){
-    document.getElementById("chat").innerHTML += chat_messages[counter++] + "<br>";
+    document.getElementById("chat").innerHTML += "<b>"+chat_messages[counter++] +"</b> <br>";
 }
